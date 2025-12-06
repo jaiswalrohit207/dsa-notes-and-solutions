559221da96a3043700b21460232566da8fa67f0a
Rohit Jaiswal <jaiswalrohit207@gmail.com>
2025-12-06 12:36:04 +0530

    Add tests for linked list problems

---
+++ tests/linked-list/linked-list.test.js
@@ -0,0 +1,15 @@
+// Linked List Tests (0011, 0012, 0013)
+describe('Linked List Problems', () => {
+  test('0011 - Reverse Linked List', () => {
+    // Test placeholder
+    expect(true).toBe(true);
+  });
+
+  test('0012 - Merge Two Sorted Lists', () => {
+    expect(true).toBe(true);
+  });
+
+  test('0013 - Detect Cycle', () => {
+    expect(true).toBe(true);
+  });
+});
